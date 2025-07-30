6a837291e12f1c6433887e506219368caf3da4b0
tomasvillani <tomasvillanidepedro2022@gmail.com>
2025-07-30 17:24:49 +0100

    primeros pasos

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
+        <title>LanzaroTCG</title>
+
+    </head>
+    <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
+        LanzaroTCG
+    </body>
+</html>
